7f1e37642f575a3abd408af8f1865965ef28bc1a
Jeet Khondker <jeetzhkhondker@gmail.com>
2023-11-20 16:59:49 +0900

    feature : Added Functionality of HTTP GET Request for Index HTML File

--- server.py
+++ server.py
@@ -1,1 +1,8 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class BPServer(BaseHTTPRequestHandler) : 
+    def do_GET(self) : 
+        
+        # Path : http://localhost:8080/
+        if self.path == '/' : 
+            self.path = "/index.html"
